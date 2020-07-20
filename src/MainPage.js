454dcda9d637d61b7fcfca6ccbb70b09928bd8c0
khanyiso <mapumak@gmail.com>
2020-07-20 19:15:46 +0200

    set validation rules with error messages

--- src/MainPage.js
+++ src/MainPage.js
@@ -13,28 +13,38 @@ const MainPage = () => {
 
     //TODO - implement a validation method of stations, simple thing like, don't pick same FROM/TO station!
     //Consider how you will manage the state of the choice in the Selection of the station from the sub-component
-    const isValid = () => undefined
-
-
+    const isValid = () => {
+        
+        if (isNaN(parseInt(from_station_id))) {
+            setError('Error: Departure Station Not Set');
+            return false;
+        } else if (isNaN(parseInt(to_station_id))) {
+            setError('Error: Arrival Station Not Set');
+            return false;
+        } else if (from_station_id === to_station_id) {
+            setError('Error: Departure station cannot be the same as Arrival Station');
+            return false;
+        }
+        setError(null);
+        return true;
+    }
 
     const click = () => {
         if (isValid()) {
             setResults([{ id: 1, name: "fast", cost: "£300" }, { id: 2, name: "medium", cost: "£200" }, { id: 3, name: "slow", cost: "£100" }]);
-        } else {
-            setError('Not a valid search');
-        }
+        } 
     }
 
     return (
-        
+
         <div className="main grid-top-container">
-            <h1>From: { from_station_id }</h1>
-        <h1>To: { to_station_id }</h1>
+            <h1>From: {from_station_id}</h1>
+            <h1>To: {to_station_id}</h1>
             <section className="search">
                 <h3>Ticket Selection</h3>
                 <div className="inputs">
                     <Selection onChange={value => setFromStation(value)} id="from-station" name="From: " options={[{ value: 1, label: "Penzance" }, { value: 2, label: "Redruth" }, { value: 3, label: "Camborne" }]} />
-                    <Selection onChange={value => setToStation(value)} id="to-station" name="To: " options={[{ value: 4, label: "St Austell" }, { value: 5, label: "Redruth" }, { value: 6, label: "Plymouth" }, { value: 7, label: "London" }]} />
+                    <Selection onChange={value => setToStation(value)} id="to-station" name="To: " options={[{ value: 4, label: "St Austell" }, { value: 2, label: "Redruth" }, { value: 6, label: "Plymouth" }, { value: 7, label: "London" }]} />
                 </div>
                 <hr />
                 <div>
